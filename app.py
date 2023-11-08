1c065c9d017b7dbf31b58eaf8426f649fb266cc5
ElseSchnabele <134046424+ElseSchnabele@users.noreply.github.com>
2023-11-08 16:27:25 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,133 @@
+import tkinter as tk
+import typing
+from tkinter import ttk
+from PIL import Image, ImageTk 
+import subprocess
+from automization import CameraFilterSynronizer
+import os
+
+#display error messages
+def display_error_message(label, message):
+    label.config(text=message)
+
+#validation output: Error messages
+def validate_entry(entry, error_message_label, validation_function, error_message):
+    value = entry.get()
+    if validation_function(value):
+        display_error_message(error_message_label, "")
+        return True
+    else:
+        display_error_message(error_message_label, error_message)
+        return False
+
+#Validate integer inputs
+def validate_integer(P):
+    if P == "":
+        return True  
+    try:
+        float(P)%1 == 0
+        return True
+    except ValueError:
+        return False
+
+#Validate Float Inputs
+def validate_float(P):
+    if P == "":
+        return True  
+    try:
+        float(P)
+        return True
+    except ValueError:
+        return False
+
+def min_smaller_max(min_and_max: typing.Tuple, error_message_label, error_message):
+    minimum = min_and_max[0].get()
+    maximum = min_and_max[1].get()
+    
+    if int(minimum)< int(maximum):
+        display_error_message(error_message_label, "")
+        return True
+    else:
+        display_error_message(error_message_label, error_message)
+        return False
+    
+#run automatization.py button
+def run_script():
+    min_wavelength = min_wavelength_spinbox.get()
+    max_wavelength = max_wavelength_spinbox.get()
+    bitdepth = bitdepth_entry.get()
+    exposure_time = exposure_time_entry.get()
+    
+    is_min_valid = validate_entry(min_wavelength_spinbox, error_message_min_wavelength, validate_integer, "Please enter a valid integer for Min. Wavelength!")
+    is_max_valid = validate_entry(max_wavelength_spinbox, error_message_max_wavelength, validate_integer, "Please enter a valid integer for Max. Wavelength")
+    is_bitdepth_valid = validate_entry(bitdepth_entry, error_message_bitdepth, validate_float, "Please enter a valid float for Bit Depth")
+    is_exposure_time_valid = validate_entry(exposure_time_entry, error_message_exposure_time, validate_float, "Please enter a valid float for Exposure time")
+
+    if is_min_valid and is_max_valid and is_bitdepth_valid and is_exposure_time_valid:
+        is_min_smaller_max = min_smaller_max((min_wavelength_spinbox, max_wavelength_spinbox), error_message_max_wavelength,  "Max. Wavelength must be bigger than Min. Wavelength!")
+        if is_min_smaller_max:
+            output = f"Min. Wavelength [nm]: {min_wavelength}\nMax. Wavelength [nm]: {max_wavelength}\nBit depth: {bitdepth}\nExposure Time [µs]: {exposure_time}"
+            output_text.config(state=tk.NORMAL)
+            output_text.delete(1.0, tk.END)
+            output_text.insert(tk.END, output)
+            output_text.config(state=tk.DISABLED)
+            #TODO: initialize CameraFilterSynronizer here and display tif file (in another window)
+        
+
+#main window
+root = tk.Tk()
+root.title("Camera Filter Synchronizer")
+
+error_message_min_wavelength = tk.Label(root, text="", foreground="red")
+error_message_max_wavelength = tk.Label(root, text="", foreground="red")
+error_message_bitdepth = tk.Label(root, text="", foreground="red")
+error_message_exposure_time = tk.Label(root, text="", foreground="red")
+
+#Spinbox for Min. Wavelength (Integer)
+label1 = tk.Label(root, text="Min. Wavelength [nm]:")
+min_wavelength_spinbox = tk.Spinbox(root, from_=0, to=10000, increment=1)
+min_wavelength_spinbox.delete(0, "end")
+min_wavelength_spinbox.insert(0, 550)
+
+
+#Spinbox for Max. Wavelength (Float)
+label2 = tk.Label(root, text="Max. Wavelength [nm]:")
+max_wavelength_spinbox = tk.Spinbox(root, from_=0, to=10000, increment=1)
+max_wavelength_spinbox.delete(0, "end")
+max_wavelength_spinbox.insert(0, 570)
+
+label3 = tk.Label(root, text="Bit depth:")
+bitdepth_entry = tk.Entry(root)
+bitdepth_entry.insert(0, 32768)
+
+label4 = tk.Label(root, text="Exposure Time [µs]:")
+exposure_time_entry = tk.Entry(root)
+exposure_time_entry.insert(0, 500000)
+
+
+run_button = tk.Button(root, text="Collect Images", command=run_script)
+
+
+output_text = tk.Text(root, state=tk.DISABLED, wrap=tk.WORD, width=40, height=10)
+
+
+label1.grid(row=0, column=0)
+min_wavelength_spinbox.grid(row=0, column=1)
+error_message_min_wavelength.grid(row=0, column=2)
+
+label2.grid(row=1, column=0)
+max_wavelength_spinbox.grid(row=1, column=1)
+error_message_max_wavelength.grid(row=1, column=2)
+
+label3.grid(row=2, column=0)
+bitdepth_entry.grid(row=2, column=1)
+label4.grid(row=3, column=0)
+error_message_bitdepth.grid(row=2, column=2)
+
+exposure_time_entry.grid(row=3, column=1)
+run_button.grid(row=4, column=0, columnspan=2)
+output_text.grid(row=5, column=0, columnspan=2)
+error_message_exposure_time.grid(row=3, column=2)
+
+
+root.mainloop()
---
+++ automization.py
@@ -0,0 +1,165 @@
+import re
+import time
+import numpy as np
+import os
+import tifffile
+import typing
+
+try:
+    from KURIOS_COMMAND_LIB import *
+except OSError as ex:
+    print("Warning:",ex)
+
+try:
+    # if on Windows, use the provided setup script to add the DLLs folder to the PATH
+    from windows_setup import configure_path
+    configure_path()
+except ImportError:
+    configure_path = None
+
+
+from thorlabs_tsi_sdk.tl_camera import TLCameraSDK
+from thorlabs_tsi_sdk.tl_mono_to_color_processor import MonoToColorProcessorSDK
+from thorlabs_tsi_sdk.tl_camera_enums import SENSOR_TYPE
+
+
+
+
+
+class CameraFilterSynronizer:
+    
+    def __init__(self, wavelengths: list, tag_bitdepth: int, tag_exposure: int) -> None:
+        self._wavelengths = wavelengths
+
+        #try to access Kurios
+        devs = KuriosListDevices()
+        print(devs)
+        if(len(devs) <= 0):
+            print('There is no devices connected')
+            exit()
+        Kurios = devs[0] #get serial number
+        hdl = KuriosOpen(Kurios[0],115200,3)
+        self._hdl = hdl
+                        #try to access camera
+        with TLCameraSDK() as sdk:
+            cameras = sdk.discover_available_cameras()
+            if len(cameras) == 0:
+                print("Error: no cameras detected!")
+
+            with sdk.open_camera(cameras[0]) as camera:
+                #  setup the camera for continuous acquisition
+                camera.frames_per_trigger_zero_for_unlimited = 0
+                camera.image_poll_timeout_ms = 2000  # 2 second timeout
+
+                # save these values to set for camera later
+                self._bit_depth = tag_bitdepth
+                self._exposure = tag_exposure
+
+                # need to save the image width and height for color processing
+                self._image_width= camera.image_width_pixels
+                self._image_height = camera.image_height_pixels
+
+                # initialize a mono to color processor if this is a color camera
+                is_color_camera = (camera.camera_sensor_type == SENSOR_TYPE.BAYER)
+                self._is_color_camera = is_color_camera
+                mono_to_color_sdk = None
+                mono_to_color_processor = None
+                if is_color_camera:
+                    mono_to_color_sdk = MonoToColorProcessorSDK()
+                    mono_to_color_processor = mono_to_color_sdk.create_mono_to_color_processor(
+                        camera.camera_sensor_type,
+                        camera.color_filter_array_phase,
+                        camera.get_color_correction_matrix(),
+                        camera.get_default_white_balance_matrix(),
+                        camera.bit_depth
+                    )
+                self._mono_to_color_sdk = mono_to_color_sdk
+                self._mono_to_color_processor = mono_to_color_processor
+
+                
+        
+    def gatherImages(self, output_dir: str, filename: str):
+        
+        # delete image if it exists
+        if os.path.exists(output_dir + os.sep + filename):
+            os.remove(output_dir + os.sep + filename)
+            
+                #try to access camera
+        with TLCameraSDK() as sdk:
+            cameras = sdk.discover_available_cameras()
+            if len(cameras) == 0:
+                print("Error: no cameras detected!")
+
+            with sdk.open_camera(cameras[0]) as camera:
+                #  setup the camera for continuous acquisition
+                camera.frames_per_trigger_zero_for_unlimited = 0
+                camera.image_poll_timeout_ms = 20000  # 20 second timeout
+                camera.arm(2)
+                camera.issue_software_trigger()
+                time.sleep(1)
+                camera.exposure_time_us = self._exposure
+                
+                #set range of wavelength and iterate
+
+                for wl in self._wavelengths:
+                    time.sleep(self._exposure*(10e-7)+0.3)
+                    KuriosSetWavelength(self._hdl, wl)
+                    frame = camera.get_pending_frame_or_null()
+                    if frame is None:
+                        raise TimeoutError("Timeout was reached while polling for a frame, program will now exit")
+                    image_data = frame.image_buffer
+                    if self._is_color_camera:
+                        # transform the raw image data into RGB color data
+                        image_data= self._mono_to_color_processor.transform_to_48(image_data, self._image_width, self._image_height).reshape(self._image_height, self._image_width, 3)
+                    
+                    scaled_image_data = ((image_data - image_data.min()) / (image_data.max() - image_data.min()) * 65535).astype(np.uint16)
+                    with tifffile.TiffWriter(output_dir + os.sep + filename, append=True) as tiff:
+                        """
+                            Setting append=True here means that calling tiff.save will add the image as a page to a multipage TIFF. 
+                        """
+                        tiff.save(data=scaled_image_data,  # np.ushort image data array from the camera
+                                compression = None# amount of compression (0-9), by default it is uncompressed (0)
+                                )
+                
+                    
+    def cleanup(self):
+        if self._is_color_camera:
+            try:
+                self._mono_to_color_processor.dispose()
+            except Exception as exception:
+                print("Unable to dispose mono to color processor: " + str(exception))
+            try:
+                self._mono_to_color_sdk.dispose()
+            except Exception as exception:
+                print("Unable to dispose mono to color sdk: " + str(exception))
+        with TLCameraSDK() as sdk:
+            cameras = sdk.discover_available_cameras()
+            if len(cameras) == 0:
+                print("Error: no cameras detected!")
+
+            with sdk.open_camera(cameras[0]) as camera:
+                if camera:
+                    camera.disarm()  
+                if self._hdl:
+                    KuriosClose(self._hdl) 
+
+                
+
+
+
+
+def main():
+    syncroniser = CameraFilterSynronizer(wavelengths=[ x + 550 for x in range(10)],
+                                        tag_bitdepth = 32768,
+                                        tag_exposure= int(5e6)
+                                        )
+    syncroniser.gatherImages(
+        output_dir = os.path.abspath(r'.'),
+        filename = 'test_series1.tif',
+        )
+    syncroniser.cleanup()
+    
+
+main()
+
+
